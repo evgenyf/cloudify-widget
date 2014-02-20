e527b2553d24103ac1b4d4077091bd886fbc3494
Evgeny Fisher <evgenyf@gigaspaces.com>
2014-02-20 16:19:06 +0200

    adding centos_ec2.sh

---
+++ setup/centos_ec2.sh
@@ -0,0 +1,35 @@
+
+execute(){
+    echo "executing $1"
+
+    ${WIDGET_HOME}/setup/utils/$1
+}
+
+echo "clone cloudify-modules"
+
+execute install_java.sh
+
+execute install_maven.sh
+
+execute install_cloudify_widget_modules.sh
+
+execute install_mysql.sh
+
+execute install_nginx.sh
+
+execute install_ruby.sh
+
+execute install_cloudify.sh
+
+execute install_play.sh
+
+
+
+execute install_node.sh
+
+execute install_monit.sh
+
+execute create_schema.sh
+
+cd ${WIDGET_HOME}/setup/utils
+execute upgrade.sh
